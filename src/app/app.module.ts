f7957a2fcc0358fe27eac6bbda338c53e813e101
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-08-23 12:21:43 +0800

    added speech api demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {SpeechRecognition} from "@ionic-native/speech-recognition";
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,68 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {SpeechRecognition} from "@ionic-native/speech-recognition";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  sub?: Subscription;
+  isSpeechSupported = false;
+  color: string = 'red';
+  matches: string[] = [];
+  langs: string[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public speechRecognition: SpeechRecognition) {
 
   }
 
+  async ngOnInit() {
+    let isSupported = await this.speechRecognition.isRecognitionAvailable();
+    console.log({isSupported});
+    if (isSupported) {
+      this.isSpeechSupported = true;
+      if (!(await this.speechRecognition.hasPermission())) {
+        await this.speechRecognition.requestPermission()
+      }
+      this.langs = await this.speechRecognition.getSupportedLanguages();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+      this.sub = null;
+    }
+    if (this.isSpeechSupported) {
+      this.speechRecognition.stopListening()
+    }
+  }
+
+  speak() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+      this.sub = null;
+    }
+    this.sub = this.speechRecognition.startListening().subscribe(
+      matches => this.onListenText(matches)
+    )
+  }
+
+  onListenText(matches: string[]) {
+    this.matches = matches;
+    if (matches.length == 0) {
+      return
+    }
+    this.color = matches[0].replace(/ /g, '');
+    if (this.color.endsWith('color')) {
+      this.color = this.color.replace(/color$/, '');
+    }
+    if (this.sub) {
+      this.sub.unsubscribe();
+      this.sub = null;
+    }
+  }
 }
